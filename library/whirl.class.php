d902931a4e5239041ccf6d74192d6cce54dfc358
Stefan Gruber <stefan_gruber2@gmx.at>
2015-08-22 14:36:06 +0200

    main update to v0.3
    class was updated to version 0.3 (see changelog in README.md for
    details)

--- library/whirl.class.php
+++ library/whirl.class.php
@@ -5,7 +5,17 @@ class Whirl {
 	var $cacheDir = 'cache';
 	var $finalImageWidth = 400;
 	var $finalImageHeight = 400;
-	var $blendMode = 'IMG_EFFECT_NORMAL';
+	var $blendMode = 'normal';
+	var $realMultiply = false;
+	var $effectList = false;
+	var $effects = false;
+	var $effectBrightnessLevel = 100;
+	var $effectContrastLevel = 100;
+	var $effectColorizeRgba = '255,0,0,1';
+	var $effectSmoothLevel = 100;
+	var $effectPixelSize = 10;
+	var $effectGammaCorrect = '1,1';
+	var $effectAdvancedPixelation = false;
 	
 	var $allResults;
 	
@@ -30,8 +40,94 @@ class Whirl {
 			$this->finalImageHeight = $options['finalImageHeight'];
 		}
 		
+		if (isset($options['effectGammaCorrect'])) {
+			$this->effectGammaCorrect = $options['effectGammaCorrect'];
+			
+			if (strpos($this->effectGammaCorrect, ',') !== false) {
+				$valueExplode = explode(',', $this->effectGammaCorrect);
+				$this->gammaInput = reset($valueExplode);
+				$this->gammaOutput = end($valueExplode);
+			} else {
+				$this->gammaInput = 1;
+				$this->gammaOutput = 1;
+			}
+			
+		}
+		
 		if (isset($options['blendMode'])) {
 			$this->blendMode = $options['blendMode'];
+		}
+		
+		if (isset($options['realMultiply'])) {
+			$this->realMultiply = $options['realMultiply'];
+		}
+		
+		if (isset($options['effectList'])) {
+			$this->effectList = str_replace(' ', '', $options['effectList']);
+			
+			$effects = array();
+			if (strpos($this->effectList, ',') !== false) {
+				$effectsExplode = explode(',', $this->effectList);
+				foreach ($effectsExplode as $effect) {
+					$effects[] = $effect;
+				}
+			} else {
+				$effects[] = $this->effectList;
+			}
+			$this->effects = $effects;
+		}
+		
+		if (isset($options['effectBrightnessLevel'])) {
+			if ($options['effectBrightnessLevel'] < -255) $options['effectBrightnessLevel'] = -255;
+			if ($options['effectBrightnessLevel'] > 255) $options['effectBrightnessLevel'] = 255;
+			
+			$this->effectBrightnessLevel = $options['effectBrightnessLevel'];
+		}
+		
+		if (isset($options['effectContrastLevel'])) {
+			$options['effectContrastLevel'] = $options['effectContrastLevel'] * -1;
+			if ($options['effectContrastLevel'] < -100) $options['effectContrastLevel'] = -100;
+			if ($options['effectContrastLevel'] > 100) $options['effectContrastLevel'] = 100;
+			
+			$this->effectContrastLevel = $options['effectContrastLevel'];
+		}
+		
+		if (isset($options['effectColorizeRgba'])) {
+			$this->effectColorizeRgba = $options['effectColorizeRgba'];
+			
+			if (strpos($this->effectColorizeRgba, ',') !== false) {
+				$valueExplode = explode(',', $this->effectColorizeRgba);
+				if (count($valueExplode) == 4) {
+					$this->colorize['red'] = $valueExplode[0];
+					$this->colorize['green'] = $valueExplode[1];
+					$this->colorize['blue'] = $valueExplode[2];
+					$this->colorize['alpha'] = $valueExplode[3];
+					
+					if ($this->colorize['red'] > 255) $this->colorize['red'] = 255;
+					if ($this->colorize['red'] < 0) $this->colorize['red'] = 0;
+					
+					if ($this->colorize['green'] > 255) $this->colorize['green'] = 255;
+					if ($this->colorize['green'] < 0) $this->colorize['green'] = 0;
+					
+					if ($this->colorize['blue'] > 255) $this->colorize['blue'] = 255;
+					if ($this->colorize['blue'] < 0) $this->colorize['blue'] = 0;
+					
+					if ($this->colorize['alpha'] > 1) $this->colorize['alpha'] = 1;
+					if ($this->colorize['alpha'] < 0) $this->colorize['alpha'] = 0;
+				}
+			}
+		}
+		
+		if (isset($options['effectSmoothLevel'])) {
+			$this->effectSmoothLevel = $options['effectSmoothLevel'];
+		}
+		
+		if (isset($options['effectPixelSize'])) {
+			$this->effectPixelSize = $options['effectPixelSize'];
+		}
+		
+		if (isset($options['effectAdvancedPixelation'])) {
+			$this->effectAdvancedPixelation = $options['effectAdvancedPixelation'];
 		}
 		
 		$this->createDir('src');
@@ -211,16 +307,109 @@ class Whirl {
 		$destY = ($baseHeight - $topHeight) / 2;
 		
 		if (function_exists('imagelayereffect')) {
-			imagelayereffect($baseImage, IMG_EFFECT_NORMAL);
+			switch ($this->blendMode) {
+				
+				default:
+				case 'normal':
+					imagelayereffect($topImage, IMG_EFFECT_NORMAL);
+					break;
+					
+				case 'replace':
+					imagelayereffect($topImage, IMG_EFFECT_REPLACE);
+					break;
+				
+				case 'overlay':
+					imagelayereffect($topImage, IMG_EFFECT_OVERLAY);
+					break;
+			}
 		}
 		
-		imagecopymerge(
-				$baseImage, // destination
-				$topImage, // source
-				// destination x and y
-				$destX, $destY,
-				// x, y, width, and height of the area of the source to copy
-				0, 0, $topWidth, $topHeight, $opacity);
+		if ($this->effects != false) {
+			// negate,grayscale,brightness,contrast,colorize,edges,emboss,gaussBlur,selectiveBlur,meanRemoval,smooth,pixelate
+			foreach ($this->effects as $effect) {
+				switch ($effect) {
+					case 'gammaCorrect':
+						if (function_exists('imagegammacorrect') && isset($this->gammaCorrect) && !empty($this->gammaCorrect)) {
+							imagegammacorrect($topImage, $this->gammaInput, $this->gammaOutput);
+						}
+						break;
+						
+					case 'negate':
+						if (function_exists('imagefilter')) {
+							imagefilter($topImage, IMG_FILTER_NEGATE);
+						}
+						break;
+						
+					case 'grayscale':
+						if (function_exists('imagefilter')) {
+							imagefilter($topImage, IMG_FILTER_GRAYSCALE);
+						}
+						break;
+						
+					case 'brightness':
+						if (function_exists('imagefilter')) {
+							imagefilter($topImage, IMG_FILTER_BRIGHTNESS, $this->effectBrightnessLevel);
+						}
+						break;
+						
+					case 'contrast':
+						if (function_exists('imagefilter')) {
+							imagefilter($topImage, IMG_FILTER_CONTRAST, $this->effectContrastLevel);
+						}
+						break;
+					
+					case 'colorize':
+						if (function_exists('imagefilter')) {
+							imagefilter($topImage, IMG_FILTER_COLORIZE, $this->colorize['red'], $this->colorize['green'], $this->colorize['blue'], $this->colorize['alpha']);
+						}
+						break;
+						
+					case 'edges':
+						if (function_exists('imagefilter')) {
+							imagefilter($topImage, IMG_FILTER_EDGEDETECT);
+						}
+						break;
+					
+					case 'emboss':
+						if (function_exists('imagefilter')) {
+							imagefilter($topImage, IMG_FILTER_EMBOSS);
+						}
+						break;
+						
+					case 'gaussBlur':
+						if (function_exists('imagefilter')) {
+							imagefilter($topImage, IMG_FILTER_GAUSSIAN_BLUR);
+						}
+						break;
+					
+					case 'selectiveBlur':
+						if (function_exists('imagefilter')) {
+							imagefilter($topImage, IMG_FILTER_SELECTIVE_BLUR);
+						}
+						break;
+						
+					case 'meanRemoval':
+						if (function_exists('imagefilter')) {
+							imagefilter($topImage, IMG_FILTER_MEAN_REMOVAL);
+						}
+						break;
+					
+					case 'smooth':
+						if (function_exists('imagefilter')) {
+							imagefilter($topImage, IMG_FILTER_SMOOTH, $this->effectSmoothLevel);
+						}
+						break;
+						
+					case 'pixelate':
+						if (function_exists('imagefilter')) {
+							imagefilter($topImage, IMG_FILTER_PIXELATE, $this->effectPixelSize, $this->effectAdvancedPixelation);
+						}
+						break;
+				}
+			}
+		}
+		
+		imagecopymerge($baseImage, $topImage, $destX, $destY, 0, 0, $topWidth, $topHeight, $opacity);
 		
 		imagepng($baseImage, $destFile);
 
@@ -271,7 +460,7 @@ class Whirl {
 			}
 		}
 		
-		if ($srcTyp == 1) { // GIF
+		if ($srcTyp == 1) {
 			$image = imagecreatefromgif ($src . '/' . $img);
 			$newImage = imagecreate ($newImgWidth, $newImgHeight);
 			imagecopyresampled ($newImage, $image, 0, 0, 0, 0, $newImgWidth, $newImgHeight, $srcWidth, $srcHeight);
@@ -280,7 +469,7 @@ class Whirl {
 			imagedestroy ($newImage);
 			return $dest . '/' . $img;
 	
-		} elseif ($srcTyp == 2) {// JPG
+		} elseif ($srcTyp == 2) {
 			$image = imagecreatefromjpeg ($src . '/' . $img);
 			$newImage = imagecreatetruecolor ($newImgWidth, $newImgHeight);
 			imagecopyresampled ($newImage, $image, 0, 0, 0, 0, $newImgWidth, $newImgHeight, $srcWidth, $srcHeight);
@@ -289,7 +478,7 @@ class Whirl {
 			imagedestroy ($newImage);
 			return $dest . '/' . $img;
 	
-		} elseif ($srcTyp == 3) {// PNG
+		} elseif ($srcTyp == 3) {
 			$image = imagecreatefrompng ($src . '/' . $img);
 			$newImage = imagecreatetruecolor ($newImgWidth, $newImgHeight);
 			imagecopyresampled ($newImage, $image, 0, 0, 0, 0, $newImgWidth, $newImgHeight, $srcWidth, $srcHeight);
